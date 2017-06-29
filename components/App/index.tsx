726d8840138cfe5eb460286088aa142f19f9d8b7
kellenren(任晓康) <rxk666666@gmail.com>
2017-06-29 18:08:28 +0800

    初始化提交

---
+++ components/App/index.tsx
@@ -0,0 +1,22 @@
+import "./index.scss"
+import * as React from "react";
+
+export interface AppProps {};
+
+export class App extends React.Component<AppProps, undefined>{
+    constructor(props: AppProps){
+        super(props);
+    }
+
+
+    componentDidMount(){
+    }
+
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ components/Container/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+
+export interface ContainerProps{
+
+}
+
+export default class Container extends React.Component<ContainerProps, undefined>{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        
+        return (<div></div>)
+    }
+}
